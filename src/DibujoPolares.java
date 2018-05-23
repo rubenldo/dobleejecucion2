ed45678c81e0d37a00ab8d7a909a09a1bc40c12b
Ruben <rubenldsmr@gmail.com>
2018-05-23 13:01:46 +0200

    Version bandera japon, bandera elche

--- src/DibujoPolares.java
+++ src/DibujoPolares.java
@@ -12,43 +12,14 @@ public class DibujoPolares {
         //El angulo empieza en la parte superior de la vertical y va en sentido antihorario
 
 
-        if (angulo <= 0.25 || angulo >= 0.75) {
-            if (radio < 0.1) {
-                return new Color(147, 147, 147);
-
+        if (angulo <= 0.75 || angulo >= 0.75) {
+            if (radio > 0.0 && radio < 0.3) {
+                return new Color(198, 4, 0);
             }
-            if (radio > 0.1 && radio < 0.2) {
-                return new Color(136, 24, 246);
-
-            }
-            if (radio > 0.2 && radio < 0.3) {
-                return new Color(21, 22, 255);
-
-            }
-            if (radio > 0.3 && radio < 0.4) {
-                return new Color(118, 238, 255);
-
-            }
-            if (radio > 0.4 && radio < 0.5) {
-                return new Color(36, 255, 102);
-
-            }
-            if (radio > 0.5 && radio < 0.6) {
-                return new Color(253, 255, 57);
-
-            }
-            if (radio > 0.6 && radio < 0.7) {
-                return new Color(255, 135, 30);
-
-            }
-            if (radio > 0.7 && radio < 0.8) {
-                return new Color(255, 21, 34);
-
-            }
-
         }
-        return new Color(147, 147, 147);
+        return new Color(255, 255, 255);
     }
+
 
 
     //NO MODIFICAR A PARTIR DE AQUÍ
